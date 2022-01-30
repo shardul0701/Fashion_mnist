24c22b65c58deae05a6acea4dcfbe7624a0567ba
shardul <shardulmehetar07@gmail.com>
2022-01-30 11:29:10 +0530

    baisc

---
+++ src/training.py
@@ -0,0 +1,14 @@
+from src.utils.common import read_config 
+import argparse
+
+def training(config_path):
+    config = read_config(config_path)
+
+    print(config)
+
+if __name__ == '__main__':
+    args = argparse.ArgumentParser()
+    args.add_argument("--config","-c",default="config.yaml")
+    parsed_args = args.parse_args()
+
+    training(config_path=parsed_args.config)
